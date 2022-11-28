e621ed0ac4e4fb9b0f078f79ee3b38a78aa2656c
UmutcanGuncu <92626158+UmutcanGuncu@users.noreply.github.com>
2022-11-28 22:02:51 +0300

    Proje Partiallara Ayrılıyor

---
+++ Core_Project/Controllers/AdminController.cs
@@ -0,0 +1,20 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace Core_Project.Controllers
+{
+    public class AdminController : Controller
+    {
+        public PartialViewResult PartialSidebar()
+        {
+            return PartialView();
+        }
+        public PartialViewResult FooterPartial()
+        {
+            return PartialView();
+        }
+        public PartialViewResult ScriptPartial() 
+        {
+            return PartialView();
+        }
+    }
+}
---
+++ Core_Project/Controllers/SkillController.cs
@@ -0,0 +1,19 @@
+﻿using BusinnesLayer.Concrete;
+using DataAccessLayer.EntityFramework;
+using EntityLayer.Concrete;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Core_Project.Controllers
+{
+    public class SkillController : Controller
+    {
+        SkillManager skillManager = new SkillManager(new EfSkillDal());
+        public IActionResult Index()
+        {
+            var values = skillManager.TGetListAll();
+            return View(values);
+        }
+       
+        
+    }
+}
